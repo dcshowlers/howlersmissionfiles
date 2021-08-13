ed20bde10934c55406f397ee78f70e91dc106994
tillmas <mtillman76@gmail.com>
2021-08-13 08:30:14 -0400

    Add files via upload

---
+++ lsoscores.lua
@@ -0,0 +1,4 @@
+Tue Aug 10 08:01:21 2021,425.8,USS Lincoln,FA-18C_hornet,Aerial-8-1,Bunny,LSO: GRADE:WO  _TMRDIC_  _LOIC_  _PIC_  _PPPIC_  WO(AFU)IC [BC]
+Thu Aug 12 21:21:58 2021,1435.618,USS Lincoln,FA-18C_hornet,Aerial-13-1,Bunny,LSO: GRADE:B  3PTSIW  (NX)  _FX_  _TMRDIC_   _LOIC_  BIW  _TMRDX_  _DRX_  _LURX_ [BC]
+Thu Aug 12 21:54:41 2021,3399.118,USS Lincoln,FA-18C_hornet,Aerial-13-1,Bunny,LSO: GRADE:WO  _DRIM_  (LURIM)  WO(AFU)IC 
+Thu Aug 12 21:56:41 2021,3519.368,USS Lincoln,FA-18C_hornet,Aerial-13-1,Bunny,LSO: GRADE:C : _LURX_  LOIC  _LOAR_  WIRE# 4[BC]
